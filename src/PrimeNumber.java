276e45e0ea16a056fa0dedf13dcf8785e6d0b43d
cuel01 <cutelhj0322gmail.com>
2024-03-14 20:46:40 +0900

    v1.0 prime number

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -3,23 +3,31 @@ import java.util.Scanner;
 public class PrimeNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
+        int numberStart = scanner.nextInt();
+        int numberEnd = scanner.nextInt();
+        int temp=0;
+        if (numberStart>numberEnd){
+            temp=numberStart;
+            numberStart=numberEnd;
+            numberEnd=temp;
+        }
         boolean isPrimeNumber = true;
 
-        if(number<2)
-            isPrimeNumber = false;
-        else {
-            for (int i = 2; i *i < number+1; i++) {
-                if (number % i == 0) {
-                    //count++
-                    isPrimeNumber = false;
-                    break; 
+        for (int k=numberStart; k<=numberEnd; k++){
+            isPrimeNumber = true;
+            if(k<2)
+                isPrimeNumber = false;
+            else {
+                for (int i = 2; i *i < k+1; i++) {
+                    if (k % i == 0) {
+                        //count++
+                        isPrimeNumber = false;
+                        break;
+                    }
                 }
             }
+            if (isPrimeNumber)
+                System.out.print(k+" ");
         }
-        if (isPrimeNumber)
-            System.out.println(number+" is a prime number!");
-        else
-            System.out.println(number+" is NOT a prime number!");
     }
 }
