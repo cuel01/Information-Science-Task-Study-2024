21206e4e29202222f5668300f0b1d78cdb3362fe
cuel01 <cutelhj0322gmail.com>
2024-03-14 19:46:46 +0900

    v0.6 primenumber

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -4,18 +4,23 @@ public class PrimeNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        int count = 0;
+        //int count = 0;
+        boolean isPrimeNumber = true;
+
         if(number<2)
-            count=-1;
+            //count=-1;
+            isPrimeNumber = false;
         else {
             for (int i = 2; i *i < number+1; i++) {
                 if (number % i == 0) {
-                    count++;
+                    //count++
+                    isPrimeNumber = false;
                     break;  // Improved performance by escaping for loop on first divisor found
                 }
             }
         }
-        if (count==0)
+        //if (count==0)
+        if (isPrimeNumber)
             System.out.println(number+" is a prime number!");
         else
             System.out.println(number+" is NOT a prime number!");
