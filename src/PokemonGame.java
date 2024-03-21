7f2528e6192fc0277fab4cce0b9974fc13d1dffd
cuel01 <cutelhj0322gmail.com>
2024-03-21 19:29:27 +0900

    v0.2 pokemon

--- src/PokemonGame.java
+++ src/PokemonGame.java
@@ -1,12 +1,25 @@
 class Pokemon{
+    //private String m_name; //prefix
     private String name;
     private int hp;
     public Pokemon(){
+        this.name="nameless";
+        this.hp=10;
         System.out.println("default constructor!");
     }
     public Pokemon(String name, int hp) {  //default constructor
-        name=name;
-        hp=hp;
+   // public Pokemon(String n, int h) {  //default constructor
+        //name=name; //remove this keyword
+        //hp=hp;
+
+        //below code works, violate readability
+        //name=n;
+        //hp=h;
+
+        //m_name=name;
+        this.name=name;
+        this.hp=hp;
+
         System.out.println("parameter constructor!");
     }
 
@@ -28,6 +41,7 @@ public class PokemonGame {
     public static void main(String[] args) {
         Pokemon p2 = new Pokemon("SQRT", 1000); //parameter constructor
         Pokemon p1 = new Pokemon(); //default constructor
+        System.out.println(p1.getName()+"'s health is "+p1.getHp()+".");
         //p1.hp=50;     // hp has private access in Pokemon
         p1.setHp(60);
         p1.setName("PPiKKaCChu");
