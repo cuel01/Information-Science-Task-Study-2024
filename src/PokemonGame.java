56d5db7a87dda0b5561603e66be7eeab3114c8a7
cuel01 <cutelhj0322gmail.com>
2024-03-28 19:10:59 +0900

    v0.5 pokemon

--- src/PokemonGame.java
+++ src/PokemonGame.java
@@ -1,7 +1,13 @@
+import pokemons.Pikachu;
 import pokemons.Pokemon;
 
 public class PokemonGame {
     public static void main(String[] args) {
+        Pikachu pika1 = new Pikachu("Pikachu",  50);
+        Pikachu pika2 = new Pikachu();
+        System.out.println(pika1.getName());
+        System.out.println(pika1.getHp());
+        /*
         Pokemon p2 = new Pokemon("SQRT", 1000);
         Pokemon p1 = new Pokemon();
         System.out.println(p1.getName()+"'s health is "+p1.getHp()+".");
@@ -9,5 +15,6 @@ public class PokemonGame {
         p1.setName("PPiKKaCChu");
         System.out.println(p1.getName()+"'s health is "+p1.getHp()+".");
         System.out.println(p2.getName()+"'s health is "+p2.getHp()+".");
+ */
     }
 }
