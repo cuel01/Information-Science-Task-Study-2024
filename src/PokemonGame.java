43dba6322da80ba965d3f3d5c05d2008c77ae741
cuel01 <cutelhj0322@gmail.com>
2024-04-04 19:28:51 +0900

    v1.9 pokemon

--- src/PokemonGame.java
+++ src/PokemonGame.java
@@ -1,25 +1,21 @@
 import pokemons.*;
 
+import java.util.Scanner;
+
 public class PokemonGame {
     public static void main(String[] args) {
-        Charizard c1 = new Charizard("liza", 1040);
-        Pikachu p1 = new Pikachu("BBiKKA", 50);
-//        p1.setFlyable(new NoFly());
-//        c1.setFlyable(new Wings());
-        p1.performFly();
-        c1.performFly();
-        Rocket rocket = new Rocket();
-        p1.setFlyable(rocket);
-        p1.performFly();
-        /*Charizard c1=new Charizard();
-        c1.setHp(1040);
-        c1.setName("liza");
-        Pikachu p1 = new Pikachu("Pikachu", 70);
-        Squirtle s2 = new Squirtle("SQRT", 90);
-        s2.attack(p1);
-        p1.attack(s2);
-        c1.attack();
-        c1.attack(p1);
-        c1.attack(s2);*/
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Choose your pokemonster. 1) Pikachu 2) SQRT 3) Charizard : ");
+        int select =scanner.nextInt();
+
+        Pokemon playerPokemon;
+        if(select==1) playerPokemon = new Pikachu();
+        else if (select==2) playerPokemon = new Squirtle();
+        else if (select==3) playerPokemon = new Charizard();
+
+        while (true) {
+            System.out.println("pass");
+            break;
+        }
     }
 }
